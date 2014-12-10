8176693b22293aa1e443a5d6e0c020c84102417d
Brindu <Brindu@Brindu-PC>
2014-12-10 14:50:16 +0200

    HelloWorld

--- twitter/src/main/java/ro/exenne/twitter/HelloBrindu.java
+++ twitter/src/main/java/ro/exenne/twitter/HelloBrindu.java
@@ -6,6 +6,6 @@ package ro.exenne.twitter;
 public class HelloBrindu {
 
     public static void main(String[] args) {
-        System.out.printf("Hello there !");
+        System.out.printf("Hello world!");
     }
 }
